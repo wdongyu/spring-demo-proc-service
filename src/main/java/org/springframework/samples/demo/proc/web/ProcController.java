fe4a35e8db80c3530992dfd2f579b8535bdd7667
wdongyu <997473700@qq.com>
2018-04-26 16:23:33 +0800

    enable a new version of service running in background

--- src/main/java/org/springframework/samples/demo/proc/web/ProcController.java
+++ src/main/java/org/springframework/samples/demo/proc/web/ProcController.java
@@ -13,13 +13,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Properties;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 /**
@@ -60,6 +64,56 @@ public class ProcController {
         //return (" ---  Process part  --- " + '\n' + fromAuth + '\n' + fromDb);
     }
 
+    @RequestMapping(value = "/servicePath" ,method = RequestMethod.GET)
+    @CrossOrigin(origins = "*")
+    public JSONObject servicePath() {
+        Properties properties = System.getProperties();
+        String path = properties.getProperty("user.dir");
+        String res = "{\"path\" : \"" + path + "\"}";
+
+        //execCommand("/usr/bin/open -a /Applications/Utilities/Terminal.app");
+        execCommand("git pull");
+        execCommand("mvn package");
+        execCommand("nohup java -jar ./target/spring-demo-proc-service-1.5.6.jar &");
+        //Process pro = null;
+        try {
+            //pro = rt.exec("mvn spring-boot:run", null, new File(path));
+            //ProcessBuilder pb = new ProcessBuilder(cmdString);
+            //pb.start();
+            return (JSONObject)(new JSONParser().parse(res));
+        } catch (Exception e) {
+            //TODO: handle exception
+            logger.info(e);
+        }
+        
+        return null;
+    }
+
+
+    private void execCommand(String command) {
+        try {
+            Process pro = Runtime.getRuntime().exec(command);
+            if (pro != null) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(pro.getOutputStream())), true);
+                try {
+                   String line;
+                   while ((line = in.readLine()) != null) {
+                      logger.info(line);
+                   }
+                   pro.waitFor();
+                   in.close();
+                   out.close();
+                   pro.destroy();
+                }
+                catch (Exception e) {
+                   logger.info("aaa");
+                }
+             }
+        } catch (Exception e) {
+            logger.info("fail to exec " + command);
+        }
+    }
 
     public String serviceUrlWithId(String serviceName, String commitId) {
         List<ServiceInstance> list = this.client.getInstances(serviceName);
